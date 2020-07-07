b136b1630ae995baaee89ea7c8be4b7f70b6642f
Kapilan Navaratnam <kapilan@syomortgage.com>
2020-07-07 02:46:32 -0400

    Added Front-End components, graphql resolvers, mongoose Schemas, authorization

--- app.js
+++ app.js
@@ -1,85 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
-const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config()
 
-
-const Event = require('./models/event');
+const graphQlSchema = require ('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', 
-    graphqlHttp({
-    schema: buildSchema(`
-    type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
     }
+    next();
+});
 
-    input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
+app.use(isAuth);
 
-    type RootQuery {
-        events: [Event!]!
-    }
-
-    type RootMutation {
-        createEvent(eventInput: EventInput): Event
-    }
-
-    schema {
-        query: RootQuery
-        mutation: RootMutation
-    }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find()
-            .then(events => {
-                return events.map(event => {
-                    return { ...event._doc, _id: event.id };
-                })
-            }).catch(err => {
-                throw err;
-            })
-        },
-        createEvent: (args) => {
-            const event = new Event ({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
-            });
-            return event
-            .save()
-            .then(result =>{
-                console.log(result);
-                return {...result._doc, _id: result._doc._id.toString() };
-            })
-            .catch(err =>{
-                console.log(err)
-                throw err;
-            });
-        }
-    },
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
-}));
+  })
+);
+
+
 
 mongoose.connect(
     `${process.env.MONGO_URL}`
     )
 .then(() => {
-    app.listen(3000);
+    app.listen(8000);
     console.log('connected');
 }).catch(err => {
     console.log(err);
